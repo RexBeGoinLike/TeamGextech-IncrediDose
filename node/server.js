14c877d2d176eb36f89e9d60f8212c66ea94e209
John Lex Paulo Palaris <pfantom39@gmail.com>
2025-11-29 11:57:03 +0800

    basic node functionalities

---
+++ node/server.js
@@ -0,0 +1,26 @@
+let mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "312-TeamGextech-mid"
+});
+
+app.get('/getpatients', (req, res) => {
+    const id = req.query.id;
+    con.query(
+        'SELECT u.userid, u.firstname, u.lastname, u.email, u.contactnum, MAX(p.dateprescribed) AS dateprescribed FROM User u JOIN Prescription p ON u.userid = p.patientid WHERE p.doctorid = ? GROUP BY u.userid',
+        [id],                      
+        (err, results) => {
+            if (err) return res.status(500).json({ error: err });
+            res.json(results);
+        }
+    );
+});
+
+app.listen(3000, () => console.log('Node API running on port 3000'));
